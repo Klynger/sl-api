7e3c5d719d313f544465ccbc0ff2fdd07d54ad41
Rafael Klynger <rafael.klynger@gmail.com>
2026-09-14 17:33:12 -0300

    Squash migrations into a single baseline schema

--- migrations/00004_users_make_username_unique.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE users ADD CONSTRAINT users_username_unique UNIQUE (username);
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE users DROP CONSTRAINT users_username_unique;
--- +goose StatementEnd
